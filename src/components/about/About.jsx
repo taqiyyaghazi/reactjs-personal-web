cc4a54b73341a3fdd11c708f817a644083326c30
Taqiyya Ghazi <taqiyyaghazi@gmail.com>
2022-04-01 11:44:32 +0700

    Add About Section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,52 @@
 import React from "react";
 import "./about.css";
+import ME from "../../assets/about-me.png";
+import { FaAward } from "react-icons/fa";
+import { FaUserFriends } from "react-icons/fa";
+import { BsFillFolderFill } from "react-icons/bs";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className="about__card">
+              <FaUserFriends className="about__icon" />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+            <article className="about__card">
+              <BsFillFolderFill className="about__icon" />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+          <p>
+            I am a full stack web developer with a passion for creating
+            beautiful. I have a background in business and marketing, and I am
+            currently.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
