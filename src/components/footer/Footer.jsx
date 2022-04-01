8852e3c63f48486c6f75b217581d339d1a0dd278
Taqiyya Ghazi <taqiyyaghazi@gmail.com>
2022-04-01 21:11:15 +0700

    Add Footer Section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,46 @@
 import React from "react";
 import "./footer.css";
+import { BsLinkedin, BsInstagram, BsGithub } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/taqiyya-ghazi/" target="_blank">
+          <BsLinkedin />
+        </a>
+        <a href="https://github.com/taqiyyaghazi" target="_blank">
+          <BsGithub />
+        </a>
+        <a href="https://www.instagram.com/zi.taqiy/" target="_blank">
+          <BsInstagram />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; 2020 Ghazi. All Right reserved.</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
