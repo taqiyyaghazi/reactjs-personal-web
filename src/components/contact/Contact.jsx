a4ba730638d7ce3add2678e8e50327cc5b7f17a0
Taqiyya Ghazi <taqiyyaghazi@gmail.com>
2022-04-01 20:48:34 +0700

    Add Form Contact Me

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
-import './contact.css'
+import React from "react";
+import "./contact.css";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
-  return (
-    <section id='contact'>Contact</section>
-  )
-}
+  const form = useRef();
 
-export default Contact
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_p7zfznl",
+      "template_8h4lt0n",
+      form.current,
+      "0EZHXJnwpRxzGcp72"
+    );
+
+    e.target.reset();
+  };
+
+  return (
+    <section id="contact">
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" class="btn btn-primary">
+            <h4>Send Message</h4>
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
