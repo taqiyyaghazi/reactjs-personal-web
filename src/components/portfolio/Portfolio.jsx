3f8560d210846624ceabd86aec455f460c9898f5
Taqiyya Ghazi <taqiyyaghazi@gmail.com>
2022-04-04 03:58:57 +0700

    Memperbarui Konten Portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -14,82 +14,98 @@ import { Autoplay, Pagination, Navigation } from "swiper";
 const data = [
   {
     id: 1,
-    image:
-      "https://images.unsplash.com/photo-1510915228340-29c85a43dcfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-    title: "Project 1",
-    desc: "Halo ini adalah deskripsi project",
-    github: "https://github.com/",
-    demo: "taqiyyaghazi.github.io",
-    view: ""
+    image: "https://taqiyyaghazi.github.io/img/projects/bbbook.png",
+    title: "Desain Brand Logo BersamaBisa Book Publishing",
+    desc: "BersamaBisa Book Publishing merupakan lembaga penerbitan dibawah naungan komunitas BersamaBisa.id.",
+    github: "",
+    demo: "",
+    view: "",
   },
   {
     id: 2,
-    image:
-      "https://images.unsplash.com/photo-1510915228340-29c85a43dcfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-    title: "Project 2",
-    desc: "Halo ini adalah deskripsi project",
-    github: "https://github.com/",
-    demo: "taqiyyaghazi.github.io",
-    view: ""
+    image: "https://taqiyyaghazi.github.io/img/projects/bbid.png",
+    title: "Desain Brand Logo BersamaBisa.id",
+    desc: "BersamaBisa.id merupakan komunitas Sociopreneur yang dibentuk oleh mahasiswa Universitas Sebelas Maret Surakarta.",
+    github: "",
+    demo: "",
+    view: "",
   },
   {
     id: 3,
-    image:
-      "https://images.unsplash.com/photo-1510915228340-29c85a43dcfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-    title: "Project 3",
-    desc: "Halo ini adalah deskripsi project",
-    github: "https://github.com/",
-    demo: "taqiyyaghazi.github.io",
-    view: ""
+    image: "https://taqiyyaghazi.github.io/img/projects/sgi.png",
+    title: "Desain Brand Logo Sekolah Generasi Indonesia",
+    desc: "Sekolah Generasi Indonesia merupakan sekolah gratis yang dibangun dibawah naungan komunitas BersamaBisa.id.",
+    github: "",
+    demo: "",
+    view: "",
   },
   {
     id: 4,
-    image:
-      "https://images.unsplash.com/photo-1510915228340-29c85a43dcfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-    title: "Project 4",
-    desc: "Halo ini adalah deskripsi project",
+    image: "https://taqiyyaghazi.github.io/img/projects/mm.png",
+    title: "Desain Cover Buku Muda Memimpin",
+    desc: "Buku Muda Memimpin merupakan karya yang ditulis oleh Shoffan Mujahid (Mahasiswa Universitas Sebelas Maret) dan diterbitkan JSI Press.",
     github: "",
-    demo: "taqiyyaghazi.github.io",
-    view: ""
+    demo: "",
+    view: "",
+  },
+  {
+    id: 5,
+    image: "https://taqiyyaghazi.github.io/img/projects/mhr.png",
+    title: "Desain Cover Buku Memimpin Hati Rakyat",
+    desc: "Buku Memimpin Hati Rakyat merupakan karya yang ditulis oleh Shoffan Mujahid (Mahasiswa Universitas Sebelas Maret) dan telah diterbitkan oleh Sabuk Pustaka.",
+    github: "",
+    demo: "",
+    view: "",
+  },
+  {
+    id: 6,
+    image: "https://taqiyyaghazi.github.io/img/projects/bfp-predictor.png",
+    title: "Body Fat Predictor App",
+    desc: "Aplikasi yang dibuat menggunakan model machine learning dan dapat mengestimasi jumlah lemak dalam tubuh menggunakan beberapa indikator.",
+    github: "",
+    demo: "",
+    view: "",
   },
 ];
 
-const codingProject = data.map(({ id, image, title, desc, github, demo, view }) => {
-  if (github !== "") {
-    return (
-      <SwiperSlide key={id} className="portfolio__item">
-        <div className="portfolio__item-image">
-          <img src={image} alt={title} />
-        </div>
-        <h3>{title}</h3>
-        <p>{desc}</p>
-        <div className="portfolio__item-cta">
-          <a href={github} className="btn" target="_blank">
-            Github
-          </a>
-          <a href={demo} className="btn btn-primary" target="_blank">
-            Live Demo
-          </a>
-        </div>
-      </SwiperSlide>
-    );
-  } else {
-    return (
-      <SwiperSlide key={id} className="portfolio__item">
-        <div className="portfolio__item-image">
-          <img src={image} alt={title} />
-        </div>
-        <h3>{title}</h3>
-        <p>{desc}</p>
-        <div className="portfolio__item-cta">
-          <a href={view} className="btn btn-primary" target="_blank">
-            View
-          </a>
-        </div>
-      </SwiperSlide>
-    );
+const codingProject = data.map(
+  ({ id, image, title, desc, github, demo, view }) => {
+    if (github !== "") {
+      return (
+        <SwiperSlide key={id} className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={image} alt={title} />
+          </div>
+          <h3>{title}</h3>
+          <p>{desc}</p>
+          <div className="portfolio__item-cta">
+            <a href={github} className="btn" target="_blank">
+              Github
+            </a>
+            <a href={demo} className="btn btn-primary" target="_blank">
+              Live Demo
+            </a>
+          </div>
+        </SwiperSlide>
+      );
+    } else {
+      return (
+        <SwiperSlide key={id} className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={image} alt={title} />
+          </div>
+          <h3>{title}</h3>
+          <p>{desc}</p>
+          <div className="portfolio__item-cta">
+            <a href={view} className="btn btn-primary" target="_blank">
+              View
+            </a>
+          </div>
+        </SwiperSlide>
+      );
+    }
   }
-});
+);
 
 const Portfolio = () => {
   return (
