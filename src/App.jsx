6daa2db96a2d2bdf27f4acc62b565257f6610510
Taqiyya Ghazi <taqiyyaghazi@gmail.com>
2022-06-02 11:47:56 +0700

    Tambah Pre-loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
-import Header from './components/header/Header'
-import Nav from './components/nav/Nav'
-import About from './components/about/About'
-import Experience from './components/experience/Experience'
-import Portfolio from './components/portfolio/Portfolio'
+import React from 'react';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import About from './components/about/About';
+import Experience from './components/experience/Experience';
+import Portfolio from './components/portfolio/Portfolio';
 
-import Contact from './components/contact/Contact'
-import Footer from './components/footer/Footer'
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
 const App = () => {
+  setTimeout(() => {
+    document.getElementById('loading').style.display = 'none';
+  }, 4000);
   return (
     <div>
-        <Header />
-        <Nav />
-        <About />
-        <Experience />
-        <Portfolio />
-        <Contact />
-        <Footer />
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Portfolio />
+      <Contact />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
